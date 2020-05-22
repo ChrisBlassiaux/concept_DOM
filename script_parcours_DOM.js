974ce0ccd7a03ac81a4d90dd04efd5904aa477d1
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-05-22 14:26:35 +0200

    first commit: DOM

---
+++ script_parcours_DOM.js
@@ -0,0 +1,5 @@
+console.log("Combien y a-t-il d'éléments <p> présents dans la page HTML ?")
+console.log(document.getElementsByTagName('p').length)
+
+console.log("Quelle est l'URL vers laquelle pointe le 3ème élément <a> de la page HTML ?")
+console.log(document.getElementsByTagName('p').length)
